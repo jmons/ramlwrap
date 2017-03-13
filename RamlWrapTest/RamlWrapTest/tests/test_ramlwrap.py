76f0027751ee3d3ac345c03d11b7002bb1a46942
James Taylor <james@proxama.com>
2017-03-13 15:10:26 +0000

    Import of RamlWrap + Swagger changes from internal project
    Cross import of new features for pre-release (tests to be patched as use internal tests at the moment).
    
    Now supports swagger.
    
    Work performed by Natalie Crowe-Lord and Jamie West.

---
+++ RamlWrapTest/RamlWrapTest/tests/test_ramlwrap.py
@@ -0,0 +1,271 @@
+import json
+
+from ramlwrap.RamlWrap import ramlwrap
+from ramlwrap.utils.validation import ExampleAPI, ValidatedPOSTAPI, ValidatedGETAPI, _is_valid_query, _example_api
+from ramlwrap.utils.raml import raml_url_patterns
+from ramlwrap.utils.swaggle import swagger_url_patterns
+from ramlwrap.utils.exceptions import FatalException
+from django.test import TestCase
+from django.test.client import RequestFactory
+from unittest import skip
+
+from jsonschema.exceptions import ValidationError
+
+import yaml
+from ramlwrap.utils.yaml_include_loader import Loader
+
+
+def _internal_mockfunc(request, example):
+    pass
+
+
+class RamlWrapTestCase(TestCase):
+
+    def test_ramlwrap_success(self):
+
+        funcmap_1 = {
+            "cms2": _internal_mockfunc,
+            "turtle": _internal_mockfunc,
+            "turtle/quote": _internal_mockfunc
+        }
+
+        funcmap_2 = {
+            "choose_pet": _internal_mockfunc,
+            "suprise_pet": _internal_mockfunc
+        }
+
+        # Test with Raml file
+        self.assertTrue(ramlwrap("RamlWrapTest/tests/fixtures/raml/test1.raml", funcmap_1))
+
+        # Test with Swagger file - references local json files
+        self.assertTrue(ramlwrap("RamlWrapTest/tests/fixtures/swagger/swagger.yaml", funcmap_2))
+
+        # Test with Swagger file - all details within same file
+        self.assertTrue(ramlwrap("RamlWrapTest/tests/fixtures/swagger/all_in_swagger.yaml", funcmap_2))
+
+    def test_ramlwrap_fail(self):
+
+        funcmap= {
+            "test1": _internal_mockfunc,
+            "test2": _internal_mockfunc,
+        }
+
+        # Test with non-existent files
+        fail_file_paths = [
+            "bad_file.yaml",
+            1234,
+            "boop",
+            "RamlWrapTest/tests/fixtures/swagger/im_not_here.yaml",
+        ]
+
+        for file_path in fail_file_paths:
+            with self.assertRaises(FatalException):
+                ramlwrap(file_path, funcmap)
+
+    def test_raml_url_patterns(self):
+
+        funcmap = {
+            "cms2": _internal_mockfunc,
+            "turtle": _internal_mockfunc,
+            "turtle/quote": _internal_mockfunc
+        }
+
+        patterns = raml_url_patterns('RamlWrapTest/tests/fixtures/raml/test1.raml', funcmap)
+
+        # These urls should now match, and have a function and default args as defined
+        urls_to_test = [
+            {
+                'url': "cms2",
+                'function': ValidatedPOSTAPI,
+                'default_args': {'target': _internal_mockfunc, 'schema': {u'$schema': u'http://json-schema.org/draft-04/schema#', u'required': [u'associatedData', u'cipherText', u'startingVariable'], u'type': u'object', u'description': u'Schema for CMS service request. See Decrypt function on pg 225 in CMS spec for more detail of contents', u'properties': {u'associatedData': {u'type': u'string', u'description': u"Associated data in format 'Sender_ID; ServiceRequest_ID; Service_ID;', See pg 128 in CMS spec for more info. Sender_ID is used to identify CCM key."}, u'cipherText': {u'type': u'string', u'description': u'Data authenticated and encrypted'}, u'startingVariable': {u'type': u'string', u'description': u'Starting Variable (nonce)'}}}}
+            },
+            {
+                'url': "cms2/foo",
+                'function': ExampleAPI,
+                'default_args': {'example': None}
+            },
+            {
+                'url': "cms",
+                'function': ExampleAPI,
+                'default_args': {'example': "{ 'data': 'foo' }"}
+            },
+            {
+                'url': "turtle",
+                'function': ValidatedGETAPI,
+                'default_args': {'target': _internal_mockfunc, 'expected_params': {'name': {'repeat': None, 'displayName': 'Name', 'name': None, 'default': None, 'pattern': None, 'enum': None, 'maximum': None, 'required': True, 'minimum': None, 'minLength': None, 'maxLength': None, 'type': 'string', 'example': 'Michelangelo', 'description': None}}}
+            },
+            {
+                'url': "cms2/foo/bar",
+                'function': ExampleAPI,
+                'default_args': {'example': None}
+            },
+            {
+                'url': "cms3/foo/bar",
+                'function': ExampleAPI,
+                'default_args': {'example': None}
+            }
+        ]
+
+        for x in urls_to_test:
+            found = False
+
+            for p in patterns:
+
+                if p.regex.match(x['url']):
+                    found = True
+                    self.assertEqual(x['function'], p.callback)
+
+                    for name, val in x['default_args'].items():
+                        self.assertIn(name, p.default_args)
+                        if type(val) == dict and type(p.default_args[name]) == dict:
+                            self.assertEqual(set(val), set(p.default_args[name]))
+                        else:
+                            self.assertEqual(val, p.default_args[name])
+
+            if not found:
+                self.fail("[%s] did not match any of the regexes" % x['url'])
+
+    def test_swagger_url_patterns(self):
+
+        funcmap = {
+            "choose_pet": _internal_mockfunc,
+            "suprise_pet": _internal_mockfunc
+        }
+
+        # Load json refs into the file
+        with open("RamlWrapTest/tests/fixtures/swagger/swagger.yaml", "r") as stream:
+            swagger_dict = yaml.load(stream, Loader)
+
+        patterns = swagger_url_patterns(swagger_dict, funcmap)
+
+        # These urls should now match, and have a function and default args as defined
+        urls_to_test = [
+            {
+                'url': "choose_pet",
+                'function': ValidatedPOSTAPI,
+                'default_args': {'target': _internal_mockfunc,
+                                 'schema': {u'required': [u'animalWanted'], u'type': u'object', u'properties': {
+                                     u'animalWanted': {u'pattern': u'^[a-zA-Z0-9-]+$', u'minimum': 3,
+                                                       u'type': u'string', u'example': u'cat'}}}
+                                 }
+            },
+            {
+                'url': "suprise_pet",
+                'function': ValidatedGETAPI,
+                'default_args': {}
+            }
+        ]
+
+        for x in urls_to_test:
+            found = False
+
+            for p in patterns:
+                if p.regex.match(x['url']):
+                    found = True
+                    self.assertEqual(x['function'], p.callback)
+
+                    for name, val in x['default_args'].items():
+                        self.assertIn(name, p.default_args)
+
+                        if type(val) == dict and type(p.default_args[name]) == dict:
+                            self.assertEqual(set(val), set(p.default_args[name]))
+
+                        else:
+                            self.assertEqual(val, p.default_args[name])
+
+            if not found:
+                self.fail("[%s] did not match any of the regexes" % x['url'])
+
+    @skip("This test needs fixing.")
+    def test_get_api(self):
+        """
+        Test that the ValidatedGETAPI is validating correctly.
+        """
+        expected_params = {
+            'name': {
+                'repeat': None,
+                'displayName': 'Name',
+                'name': None,
+                'default': None,
+                'pattern': None,
+                'enum': None,
+                'maximum': None,
+                'required': True,
+                'minimum': None,
+                'minLength': 5,
+                'maxLength': None,
+                'type': 'string',
+                'example': 'Michelangelo',
+                'description': None
+            }
+        }
+        rf = RequestFactory()
+        request = rf.get("/turtle/?name=Michelangelo")
+        self.assertEqual(_is_valid_query(request.GET, expected_params), True)
+
+        request = rf.get("/turtle/?name=Mike")
+        with self.assertRaises(ValidationError):
+            _is_valid_query(request.GET, expected_params)
+
+        request = rf.get("/turtle/")
+        with self.assertRaises(ValidationError):
+            _is_valid_query(request.GET, expected_params)
+
+        expected_params = {
+            'name': {
+                'repeat': None,
+                'displayName': 'Name',
+                'name': None,
+                'default': None,
+                'pattern': None,
+                'enum': None,
+                'maximum': None,
+                'required': True,
+                'minimum': None,
+                'minLength': 5,
+                'maxLength': None,
+                'type': 'string',
+                'example': 'Michelangelo',
+                'description': None
+            },
+            'type': {
+                'repeat': None,
+                'displayName': 'Quote type',
+                'name': None,
+                'default': None,
+                'pattern': None,
+                'enum': None,
+                'maximum': None,
+                'required': False,
+                'minimum': None,
+                'minLength': 4,
+                'maxLength': None,
+                'type': 'string',
+                'example': 'funny',
+                'description': None
+            }
+        }
+
+        request = rf.get("/turtle/quote/?name=Michelangelo")
+        self.assertEquals(_is_valid_query(request.GET, expected_params), True)
+
+    def test_example(self):
+        """
+        Testing the do thing function of ExampleAPI
+        """
+
+        data = '{"splat":"splat"}'
+
+        response = _example_api(None, None, data)
+
+        self.assertEqual(json.loads(data), response)
+
+    def test_empty_post(self):
+        """
+        Testing that RAMLWRAP can handle a an empty post request.
+        """
+
+        response = _example_api(None, None, None)
+        self.assertEqual(response, None)
+
+
---
+++ ramlwrap/utils/validation.py
@@ -0,0 +1,103 @@
+import json
+import logging
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
+from . exceptions import FatalException
+
+logger = logging.getLogger(__name__)
+
+
+@api_view(["POST"])
+def ExampleAPI(request, schema, example):
+
+    return Response(_example_api(request, schema, example))
+
+
+def _example_api(request, schema, example):
+
+    if schema:
+        data = json.loads(request.body)
+        validate(data, schema)
+
+    if not example:
+        return None
+    else:
+        return json.loads(example)
+
+
+def _is_valid_query(params, expected_params):
+    """
+    Function to validate get request params.
+    """
+
+    # If expected params, check them. If not, pass.
+    if expected_params:
+        for param in expected_params:
+            # If the expected param is in the query.
+            if param in params:
+                for check, rule in expected_params[param].__dict__.items():
+                    if rule is not None:
+                        error_message = "QueryParam [%s] failed validation check [%s]:[%s]" % (param, check, rule)
+                        if check == "minLength":
+                            if len(params.get(param)) < rule:
+                                raise ValidationError(error_message)
+                        elif check == "maxLength":
+                            if len(params.get(param)) > rule:
+                                raise ValidationError(error_message)
+            # Isn't in the query but it is required, throw a validation exception.
+            elif expected_params[param].required is True:
+                raise ValidationError("QueryParam [%s] failed validation check [Required]:[True]" % param)
+
+    # TODO Add more checks here.
+    return True
+
+
+@api_view(["GET"])
+def ValidatedGETAPI(request, expected_params, target):
+    """
+    Validate GET APIs.
+    """
+
+    if _is_valid_query(request.GET, expected_params):
+        return target(request)
+
+
+@api_view(["POST"])
+def ValidatedPOSTAPI(request, schema, expected_params, target):
+    """
+    Validate POST APIs.
+    """
+
+    if expected_params:
+        _is_valid_query(request.GET, expected_params)   # Either passes through or raises an exception.
+
+    if schema:
+        # If there is a problem with the json data, return a 400.
+        try:
+            data = json.loads(request.body)
+        except Exception as e:
+            raise FatalException("Malformed JSON in the request.", 400)
+
+        # Else if there is a problem with the  json schema validation, return a 422 and the reason.
+        try:
+            validate(data, schema)   # this will throw an exception if it doesn't validate.
+        except ValidationError as e:
+            message = "Validation failed. {}".format(e.message)
+            error_response = {
+                "message": message,
+                "code": e.validator
+            }
+            logger.info(message)
+            return Response(error_response, status=422)
+    else:
+        data = json.loads(request.body.decode('utf-8'))
+
+    # Add validated data to request
+    request.validated_data = data
+
+    return target(request)
