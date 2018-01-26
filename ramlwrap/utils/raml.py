ec2930a2e9b4711ed9ffd581bfadc96a5fb5086f
James Taylor <jtaylor@cpicardgroup.com>
2018-01-26 16:12:54 +0000

    Major rewrite of parser file
    Currently WIP as some tests fail: investigating.

--- ramlwrap/utils/raml.py
+++ ramlwrap/utils/raml.py
@@ -1,8 +1,9 @@
 import logging
-import pyraml.parser
+import yaml
+from yaml_include_loader import Loader
 from django.conf.urls import url
 
-from . validation import Action, ContentType, Endpoint
+from . validation import Endpoint, Action
 
 logger = logging.getLogger(__name__)
 
@@ -16,87 +17,109 @@ def raml_url_patterns(raml_filepath, function_map):
     :return:
     """
 
-    tree = pyraml.parser.load(raml_filepath)
+    # This function will run in thtree phases: 
+    # 1) Load the raml (as a yaml document)
+    # 2) Parse the raml into nodes that represent 'endpoints'
+    # 3) Convert endpoints into a url structure
 
-    resource_map = _get_resource_for_tree(tree)
+    # migrating from pyraml: file handling now has to be done by us
+    # worry about streaming files in future version (for VERY BIG raml?)
+    f = open(raml_filepath)
+    tree = yaml.load(f, Loader=Loader) # This loader has the !include directive 
+    f.close()
 
-    patterns = _generate_patterns(resource_map, function_map)
-
-    return patterns
-
-
-def _generate_patterns(resource_map, function_map):
+    # The resource map is the found nodes
 
     patterns = []
+    to_look_at = [
+        {
+            "node" : tree,
+            "path" : ""
+        }
+    ]
 
-    for t_url, resource in resource_map.items():
-        t_url = t_url[1:]   # String leading /
-        endpoint = Endpoint(t_url)
+    # FIXME: get baseuri, and default media types out here
 
-        if resource.methods is not None:
-            if "post" in resource.methods:
-                action = Action()
+    defaults = {
+        "content_type": "application/json",
+    }
 
-                if resource.methods['post'].queryParameters:
-                    action.query_parameter_checks = dict(resource.methods['post'].queryParameters)
-                if resource.methods['post'].body:
-                    if resource.methods['post'].body['application/json']:
-                        action.requ_content_type = ContentType.JSON
-                        if resource.methods['post'].body['application/json'].schema:
-                            action.schema = resource.methods['post'].body['application/json'].schema
-
-                if resource.methods['post'].responses:
-                    if resource.methods['post'].responses[200]:
-                        if resource.methods['post'].responses[200].body:
-                            if resource.methods['post'].responses[200].body['application/json']:
-                                action.resp_content_type = ContentType.JSON
-                                if resource.methods['post'].responses[200].body['application/json'].example:
-                                    action.example = resource.methods['post'].responses[200].body['application/json'].example
-
-                if t_url in function_map:
-                    action.target = function_map[t_url]
-                endpoint.add_action('POST', action)
-
-            if "get" in resource.methods:
-                action = Action()
-                if resource.methods['get'].queryParameters:
-                    action.query_parameter_checks = dict(resource.methods['get'].queryParameters)
-                if resource.methods['get'].responses:
-                    if resource.methods['get'].responses[200]:
-                        if resource.methods['get'].responses[200].body:
-                            if resource.methods['get'].responses[200].body['application/json']:
-                                action.resp_content_type = ContentType.JSON
-                                if resource.methods['get'].responses[200].body['application/json'].example:
-                                    action.example = resource.methods['get'].responses[200].body['application/json'].example
-
-                if t_url in function_map:
-                    action.target = function_map[t_url]
-                endpoint.add_action('GET', action)
-
-            patterns.append(url("^%s$" % t_url, endpoint.serve))
+    for item in to_look_at:
+        _parse_child(item, patterns, to_look_at, function_map, defaults)
 
     return patterns
 
 
-def _get_resource_for_tree(tree):
+def _parse_child(resource, patterns, to_look_at, function_map, defaults):
+    node = resource['node']
+    path = resource['path']
+    localEndpoint = None
 
-    resource_map = {}
-    to_look_at = []
+    for k in node:
+        
+        if k.startswith("/"):
+            
+            item = {
+                "node": node[k],
+                "path": "%s%s" % (path, k)
+            }
 
-    _parse_child(tree, resource_map, to_look_at, "")
+            to_look_at.append(item)
 
-    for item in to_look_at:
-        _parse_child(resource_map[item], resource_map, to_look_at, resource_map[item].path)
+        else:
+            # attribute not subpath
+            # FIXME: deal with other headers in future ? (unit tests!)
+            
+            if k in ("get", "post"):
+                act = node[k]
+                if localEndpoint == None:
+                    localEndpoint = Endpoint(path[1:])
 
-    return resource_map
+                # FIXME: this bit is a rewrite to do dynamicness from mapping
+                # look for a 200.body.{{content-type}}
+                # and a 200.body.{{content-type}}.example
 
+                a = Action()
+                a.resp_content_type = defaults["content_type"]
 
-def _parse_child(resource, resource_map, to_look_at, path):
+                # FIXME: at some point allow a construct for multi-methods
+                if path in function_map:
+                    a.target = function_map[path] 
+                
+                if 'body' in act:
+                    # if body, look for content tpye.
+                    a.requ_content_type = next(iter(act['body']))
 
-    # This has been carefully designed so as to not use recursion.
-    if resource.resources is not None:
-        for next in resource.resources:
-            res = resource.resources[next]
-            res.path = "%s%s" % (path, next)
-            resource_map[res.path] = res
-            to_look_at.append(res.path)
+                # This horreendous if blocks are to get around none type erros when the tree
+                # is not fully built out.
+
+                if 'responses' in act and act['responses'] != None:
+                    for status_code in act['responses']:
+                        # this is a response that we care about:
+                        
+                        if status_code == 200:
+                            two_hundred = act['responses'][200]
+                            for resp_attr in two_hundred:
+                                if resp_attr == "body":
+                                    # not sure if this can fail and be valid raml?
+                                    a.resp_content_type = next(iter(two_hundred['body']))
+                                    if two_hundred['body'][a.resp_content_type]:
+                                        if "example" in two_hundred['body'][a.resp_content_type]:
+                                            a.example = two_hundred['body'][a.resp_content_type]['example']
+ 
+                                        if "schema" in two_hundred['body'][a.resp_content_type]:
+                                            a.schema = two_hundred['body'][a.resp_content_type]['schema']
+                                    
+                                elif resp_attr == "queryParameters":
+                                    # FIXME: does this help in the query parameterising?
+                                    # For filling out a.queryparameterchecks
+                                    a.query_parameter_checks= two_hundred['queryParameters']
+
+                if k == "get":
+                    localEndpoint.add_action("GET", a)
+                elif k == "post":
+                    localEndpoint.add_action("POST", a)
+
+    if localEndpoint != None:
+        # strip leading
+        patterns.append(url("^%s$" % path[1:], localEndpoint.serve))
