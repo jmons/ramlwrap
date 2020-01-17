16548d6fa54876bfa6d22be3d92fcef8f850764d
James Taylor <jtaylor@cpicardgroup.com>
2020-01-17 14:41:47 +0000

    Tagging for Release 2.1.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
 
-    version='2.1.0rc1',
+    version='2.1.1',
 
     description='Raml API mapping toolkit for Django',
     long_description=long_description,
